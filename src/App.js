0744f10619fc48ece292cc7351335c65fcc7405c
Hilmi Erdem Keren <erdemkeren@gmail.com>
2019-08-22 01:06:24 +0300

    feat: Creates the game chore: Adds gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+import _ from 'lodash';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const Number = ({ n, isEliminated, isAGuess, isFoundAnswer, ...rest }) =>
+    <span style={{
+      display: 'inline-block',
+      width: 40,
+      height: 40,
+      color: isFoundAnswer ? 'green' : isAGuess ? 'red' : isEliminated ? 'lightgray' : 'black',
+      fontSize: 12,
+    }} {...rest}>{n}</span>
+
+const range = _.range(1, 301)
+const needle = _.sample(range)
 
 function App() {
+  const [knownMin, setKnownMin] = React.useState(1)
+  const [knownMax, setKnownMax] = React.useState(_.max(range))
+  const nextSuggestion = Math.trunc((knownMax+knownMin)/2)
+  const [numbers, setNumbers] = React.useState(range.reduce((acc, n) => {
+    acc.push({
+      n: n,
+      isEliminated: false,
+      isAGuess: false,
+    })
+
+    return acc
+  }, []))
+
+  const guess = n => {
+    setNumbers(numbers.map(num => {
+      return num.n === n
+        ? {...num, isAGuess: true}
+        : {...num, isEliminated: num.isEliminated || (((n > needle && num.n > n) || (n < needle && num.n < n)) ? true : false)}
+    }))
+
+    if (n > needle) {
+      setKnownMax(n)
+    } else if(n < needle) {
+      setKnownMin(n)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{maxWidth: 1000, margin: '0 auto 30px auto' }}>
+        <h1>Guess The Number</h1>
+        <p>This game was created for educational purposes.</p>
+        <p>You can try any algorithm you want to apply to find the hidden number. Following the number list, you can find my suggestion!</p>
+        <p>If you don't have a better idea, I give you my word. We will find the number on {Math.ceil(Math.log2(range.length))} <code>round(log2(300))</code> tries at most.</p>
+      </div>
+      <div style={{display: 'flex', flexWrap: 'wrap', maxWidth: 1000, margin: '0 auto' }}>
+        { numbers.map(num => <Number key={num.n} onClick={() => {! num.isEliminated && guess(num.n)}} {...num} isFoundAnswer={needle === num.n && num.isAGuess} />)}
+      </div>
+      <div style={{ textAlign: 'center' }}>Binary search suggestion: <span style={{ color: 'blue', fontWeight: 'bold' }}>{nextSuggestion}</span></div>
+    </>
   );
 }
 
